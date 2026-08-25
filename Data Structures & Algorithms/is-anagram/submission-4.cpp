0273bb678a25745ed5a02936960533c8894b4710
rishit_0801 <154057402+CodeMaestroRishit@users.noreply.github.com>
2026-08-25 20:44:10 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())return false;
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<s.size();i++)
+        {
+        mp[s[i]]++;
+        mp[t[i]]--;
+        }
+
+        for(auto p:mp)
+        {
+            if(p.second>0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
